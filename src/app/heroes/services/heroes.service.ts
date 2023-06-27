ba48c75d6c194800570b660fe6dac19ca97edf3b
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-06-26 23:21:32 -0300

    section 192 finished

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from 'src/environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHeroes():Observable<Hero[]> {
+
+    return this.httpClient.get<Hero[]>( this.baseUrl + '/heroes' )
+
+  }
+  
+}
+
