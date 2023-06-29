5e19e7e04a3386bee0f601a19f5888f0431ce529
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-06-28 22:54:22 -0300

    section 195 finished

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -14,10 +14,15 @@ export class HeroesService {
   constructor(private httpClient: HttpClient) { }
 
   getHeroes():Observable<Hero[]> {
-
     return this.httpClient.get<Hero[]>( this.baseUrl + '/heroes' )
-
   }
   
+  getHeroById( id:string ): Observable<Hero | undefined> {
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( error => of(undefined) )
+      )
+  }
+
 }
 
