8f80b77aaf2228c39f2fd7cf12f1bc0f57fd8efb
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-06-30 01:21:23 -0300

    section 198 finished

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -11,9 +13,28 @@ import { FormControl } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl('')
+  public heroes: Hero[] = []
+  public selectedHero?: Hero 
 
   constructor(public heroesService: HeroesService) {
 
+  }
+  
+  searchHero() {
+    const value: string = this.searchInput.value || ''
+    this.heroesService.getSuggestions( value )
+      .subscribe( heroes => this.heroes = heroes )
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent ) {
+    if ( !event.option.value ) {
+      this.selectedHero = undefined
+      return
+    }
+
+    const hero: Hero = event.option.value
+    this.searchInput.setValue( hero.superhero)
+    this.selectedHero = hero
   }
 
 }
