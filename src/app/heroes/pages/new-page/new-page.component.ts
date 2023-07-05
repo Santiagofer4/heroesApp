34034c9d79a473e9501b46836733d12f6053338d
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-04 23:08:37 -0300

    section 213 finished

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
 import { Hero } from '../../interfaces/hero.interface';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { filter, switchMap } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
@@ -86,13 +86,22 @@ export class NewPageComponent implements OnInit {
       data: this.heroForm.value
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if( !result ) return
+    dialogRef.afterClosed()
+      .pipe(
+        filter(( result: boolean ) => result ),
+        switchMap( () => this.heroesService.deleteHeroById( this.currentHero.id )),
+        filter(( wasDeleted: boolean ) => wasDeleted ),
+      )
+      .subscribe(() => { this.router.navigate(['/heroes']) })
 
-      this.heroesService.deleteHeroById( this.currentHero.id )
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if( !result ) return
 
-      this.router.navigate(['/heroes'])
-    });
+    //   this.heroesService.deleteHeroById(this.currentHero.id)
+    //     .subscribe((wasDeleted) => {
+    //       if (wasDeleted)  this.router.navigate(['/heroes']);
+    //     });
+    // });
     
   }
 
